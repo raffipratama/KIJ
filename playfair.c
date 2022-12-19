f13946f2f78975defff0a0cd30dbd91af13a78f6
raffipratama <73654934+raffipratama@users.noreply.github.com>
2022-12-19 14:21:54 +0700

    update spasi

--- playfair.c
+++ playfair.c
@@ -9,16 +9,16 @@ void play_fair();
 char key[100], play[5][5], ct[255], msg[255], plain_text[255], c_text[255];
 int main(){
     int i, choice, flag=0, j, k, size;
-    printf("\n --- play Fair Cipher ---\n");
-    printf("Enter plain text :");
+    printf("\n --- Playfair Cipher Encryption ---\n");
+    printf(" Masukkan Plain Text : ");
     scanf("%[^\n]", msg);
     size=strlen(msg);
-    size=strlen(msg);
     int count = 0;
-    for (i=0; msg[i]; i++)
-    if (msg[i]!=' ')
-        msg[count++]==msg[i];
-    msg[count]='\0';
+    for (i = 0; i < size; i++)
+	if (msg[i] != ' ')
+		msg[count++] = msg[i];
+	msg[count] = '\0';
+    printf("\n  Plain text setelah dihapus spasi : %s", msg);
     for (i=0; i<size; i+=2){
         if(msg[i]==msg[i+1])
         {
@@ -33,11 +33,11 @@ int main(){
     size=strlen(msg);
     if(size%2!=0)
     {
-        msg[size]='x';
+        msg[size]='z';
         msg[size+1]='\0';
     }
-    printf("\n plaintext after space remove : %s", msg);
-    printf("\n message for enryption is : ");
+    printf("\n Plaintext setelah dikenai aturan playfair : %s", msg);
+    printf("\n Pasangan huruf yang akan dienkripsikan : ");
     for(i=0,j=0; msg[i];i++){
         putchar(msg[i]);
         if(i%2!=0)
@@ -48,7 +48,6 @@ int main(){
 
 char encryption(char pt[]){
     int i, j, k, l, r1,r2, c1, c2, p, q;
-    printf("\n Plain text : %s",pt);
     play_fair();
     j=1;
     for(i=0; i<strlen(pt);){
@@ -63,9 +62,9 @@ char encryption(char pt[]){
         for(k=0; k<5;k++){
             for(l=0; l<5;l++){
                 if(play[k][l]==p)
-                    r1=k;c1=l;
+                    {r1=k;c1=l;}
                 if(play[k][l]==q)
-                    r2=k;c2=l;
+                    {r2=k;c2=l;}
             }
         }
         if(r1==r2){
@@ -78,23 +77,24 @@ char encryption(char pt[]){
             ct[i]=play[(r1+1)%5][c1];
             ct[j]=play[(r2+1)%5][c2];
             putchar(ct[i]);
-            putchar(ct[j]);
+            putchar(ct[j]);   
         }
         else{
             ct[i]=play[r1][c2];
             ct[j]=play[r2][c1];
-            putchar(ct[i]);putchar(ct[j]);
+            putchar(ct[i]);
+            putchar(ct[j]);
         }
         i=i+2;
         j=j+2;
     }
-    printf("\n\n \tEncrypted msg is : %s\n",ct);
+    printf("\n\n \tPesan yang dienkripsikan : %s\n",ct);
     return ct;
 }
 
 void play_fair(){
     int i, k, j,x, y, size;
-    printf("\n enter key: ");
+    printf("\n Masukkan kunci : ");
     scanf("%s", key);
     strcat(key,"abcdefghiklmnopqrstuvwxyz");
     size=strlen(key);
